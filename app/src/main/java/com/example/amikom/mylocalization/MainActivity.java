82f0f8bf8f90f0ea0a2129efe0f3fa16b99ea3c3
harish0048 <harish.0048@students.amikom.ac.id>
2018-10-16 09:00:52 +0700

    first init

---
+++ app/src/main/java/com/example/amikom/mylocalization/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.amikom.mylocalization;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.amikom.mylocalization.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button lang = (Button) findViewById(R.id.btnLang);
+                lang.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                        startActivity(intent);
+                    }
+                });
+
+
+    }
+}
